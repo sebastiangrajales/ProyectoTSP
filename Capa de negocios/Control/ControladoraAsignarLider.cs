7956ce331aa3887c47fef6ddeabaee63531cdcd9
CristianCamiloAgudelo <cristian.1701322386@ucaldas.edu.co>
2019-09-29 23:06:00 -0500

    Modificacion en controladoraAsignarLider y ControladoreaAsignarRol

--- Capa de negocios/Control/ControladoraAsignarLider.cs
+++ Capa de negocios/Control/ControladoraAsignarLider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capa_de_negocios.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Capa_de_negocios.Control
 {
     class ControladoraAsignarLider
     {
+        String liderEquipo = "LiderEquipo";
+
+        public Boolean AsignarLider(Usuario usuario)
+        {
+            usuario.rol = liderEquipo;
+            return true;
+        }
+
     }
 }
